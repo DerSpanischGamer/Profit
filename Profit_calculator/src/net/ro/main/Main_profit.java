8e1eefd57a71a47a6c984d3a3abc9be01097e2e5
DerSpanischGamer <robertogc99@hotmail.com>
2017-06-29 15:25:32 +0200

    Ya hay ventana

---
+++ Profit_calculator/src/net/ro/main/Main_profit.java
@@ -0,0 +1,17 @@
+package net.ro.main;
+
+import javax.swing.JFrame;
+
+public class Main_profit
+{
+	JFrame ventana;
+	
+	public void init_ventana()
+	{
+		ventana = new JFrame();
+		
+		ventana.setSize(1280, 720);
+		ventana.setResizable(false);
+		ventana.setVisible(true);
+	}
+}
